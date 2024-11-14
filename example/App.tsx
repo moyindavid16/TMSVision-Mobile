1cf3f625ea8fa09acbd741b54e4cbdbe1644f99a
moyindavid16 <moyindavid16@gmail.com>
2024-11-14 01:41:32 -0600

    camera and build setup working

--- example/App.tsx
+++ example/App.tsx
@@ -1,20 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as Tmsvision from 'tmsvision';
+import { Button, StyleSheet, Text } from "react-native";
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from "react-native-vision-camera";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>{Tmsvision.getTheme()} still having issues</Text>
-    </View>
-  );
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  if (!hasPermission) return <Button title="Request permission" onPress={requestPermission} />;
+  if (device == null) return <Text>Camera not available</Text>;
+  return <Camera style={StyleSheet.absoluteFill} device={device} isActive />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
   },
 });
