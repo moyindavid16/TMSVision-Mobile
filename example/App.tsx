68f5f640ed996bd6f1e6049f1c95e2d63733bd10
moyindavid16 <moyindavid16@gmail.com>
2025-04-16 03:42:07 -0500

    refactor: seperate camera

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,4 @@
 import { Picker } from "@react-native-picker/picker";
-import { Skia } from "@shopify/react-native-skia";
 import { useEffect, useState } from "react";
 import {
   Button,
@@ -11,27 +10,15 @@ import {
   TextInput,
   View,
 } from "react-native";
-import { OpenCV } from "react-native-fast-opencv";
 import {
-  Camera,
   useCameraDevice,
-  useCameraFormat,
-  useCameraPermission,
-  useSkiaFrameProcessor,
+  useCameraPermission
 } from "react-native-vision-camera";
 import { useSharedValue, Worklets } from "react-native-worklets-core";
-import { useResizePlugin } from "vision-camera-resize-plugin";
 
-import { calculateRelativeVectors } from "./helpers/calculateRelativeVectors";
-import { drawFacialTriangle } from "./helpers/drawFacialTriangle";
-import { drawVectorPoints } from "./helpers/drawVectorPoints";
-import { getContourRectangles } from "./helpers/getContourRectangles";
-import { getDistanceFeedback } from "./helpers/getDistanceFeedback";
-import { getMarkerTiltDirections } from "./helpers/getMarkerTiltDirections";
-import { isFacialTriangleAligned } from "./helpers/isFacialTriangleAligned";
-import { useVisionLandmarkDetector } from "./ios/VisionFrameProcessor/visionLandmarkDetector";
+import CameraFeed from "./components/CameraFeed";
 import { getItem, setItem } from "./utils/AsyncStorage";
-import { CalibratedPoints, Point, Points } from "./utils/interfaces";
+import { CalibratedPoints } from "./utils/interfaces";
 
 export default function App() {
   const device = useCameraDevice("front");
@@ -52,18 +39,6 @@ export default function App() {
   const sharedRelativeVectors = useSharedValue<{ x: number; y: number }[][]>(
     [],
   );
-
-  const format = useCameraFormat(device, [
-    {
-      videoResolution: {
-        width: 2016,
-        height: 1512,
-      },
-    },
-    {
-      fps: 60,
-    },
-  ]);
 
   // Modify resetCalibratedValues
   const resetCalibratedValues = async () => {
@@ -126,208 +101,12 @@ export default function App() {
     setShowCalibrationModal(false);
   };
 
-  const { resize } = useResizePlugin();
-  const { detectVisionLandmarks } = useVisionLandmarkDetector();
-
   const safeSetFeedBackText = Worklets.createRunOnJS(setFeedbackText);
   const safeSetFaceAlignedText = Worklets.createRunOnJS(setFaceAlignedText);
   const safeSetItem = Worklets.createRunOnJS(setItem);
   const safeSetShowCalibrationModal = Worklets.createRunOnJS(
     setShowCalibrationModal,
   );
-
-  const skiaFrameProcessor = useSkiaFrameProcessor((frame) => {
-    "worklet";
-    // Prepare paint for circles
-    const paint = Skia.Paint();
-    paint.setStrokeWidth(5);
-    paint.setColor(Skia.Color("yellow"));
-    paint.setAntiAlias(true); // Make the circles smooth
-
-    frame.render();
-
-    // Detect landmarks
-    const points: Points = detectVisionLandmarks(frame);
-    if (!points.leftEye || !points.rightEye || !points.nose) {
-      safeSetFeedBackText(
-        "No face detetcted. Improve lighting or position face well",
-      );
-      return;
-    }
-
-    const eyeLevel =
-      Math.min(points.leftEyeCenter[0].y, points.rightEyeCenter[0].y) / 4;
-    const height = frame.height / 4;
-    const width = frame.width / 4;
-
-    const resized = resize(frame, {
-      scale: {
-        width,
-        height,
-      },
-      pixelFormat: "bgr",
-      dataType: "uint8",
-    });
-
-    //get green points
-    const greenPointsRects = getContourRectangles(
-      height,
-      width,
-      resized,
-      eyeLevel,
-    );
-    const greenPoints: Point[] = [];
-
-    for (const rectangle of greenPointsRects) {
-      greenPoints.push({ x: rectangle.x * 4, y: rectangle.y * 4 });
-
-      frame.drawRect(
-        {
-          height: rectangle.height * 4,
-          width: rectangle.width * 4,
-          x: rectangle.x * 4,
-          y: rectangle.y * 4,
-        },
-        paint,
-      );
-    }
-
-    OpenCV.clearBuffers();
-
-    safeSetFeedBackText("");
-
-    paint.setColor(Skia.Color("blue"));
-    // Draw circles at each point
-    // const allPoints = [...points.leftEye, ...points.rightEye, ...points.nose];
-    // for (const point of allPoints) {
-    //   frame.drawCircle(point.x, point.y, 5, paint); // Draw a circle with a radius of 5
-    // }
-
-    paint.setColor(Skia.Color("purple"));
-    const rightEyeCenter = points.rightEyeCenter[0];
-    const leftEyeCenter = points.leftEyeCenter[0];
-    const noseCenter = points.noseCenter[0];
-    const noseLowerCenter = points.noseLowerCenter[0];
-    const noseUpperCenter = points.noseUpperCenter[0];
-    const boxArea = points.boundingBox[0].area;
-    // frame.drawCircle(rightEyeCenter.x, rightEyeCenter.y, 5, paint);
-    // frame.drawCircle(leftEyeCenter.x, leftEyeCenter.y, 5, paint);
-    // frame.drawCircle(noseCenter.x, noseCenter.y, 5, paint);
-    // frame.drawCircle(noseLowerCenter.x, noseLowerCenter.y, 5, paint);
-    // frame.drawCircle(noseUpperCenter.x, noseUpperCenter.y, 5, paint);
-    paint.setColor(Skia.Color("green"));
-    drawFacialTriangle(paint, frame, [
-      leftEyeCenter,
-      rightEyeCenter,
-      noseLowerCenter,
-    ]);
-
-    greenPoints.sort((a, b) => a.y - b.y);
-
-    // Calculate vectors from facial landmarks to each green point
-    const relativeVectors = calculateRelativeVectors(
-      greenPoints,
-      leftEyeCenter,
-      rightEyeCenter,
-      noseLowerCenter,
-    );
-
-    if (sharedShouldCalibrate.value) {
-      sharedShouldCalibrate.value = false;
-      safeSetShowCalibrationModal(true);
-
-      const cpoints = {
-        rightEyeCenter,
-        leftEyeCenter,
-        noseCenter,
-        noseLowerCenter,
-        noseUpperCenter,
-      };
-
-      sharedCalibratedPoints.value = cpoints;
-      sharedCalibratedBoxArea.value = boxArea;
-      sharedRelativeVectors.value = relativeVectors;
-    }
-
-    if (sharedCalibratedPoints.value) {
-      paint.setColor(Skia.Color("yellow"));
-
-      const {
-        leftEyeCenter: calibratedLeftEyeCenter,
-        rightEyeCenter: calibratedRightEyeCenter,
-        noseLowerCenter: calibratedNoseLowerCenter,
-      } = sharedCalibratedPoints.value;
-
-      drawFacialTriangle(paint, frame, [
-        calibratedLeftEyeCenter,
-        calibratedRightEyeCenter,
-        calibratedNoseLowerCenter,
-      ]);
-
-      const isAligned = isFacialTriangleAligned(
-        { leftEyeCenter, rightEyeCenter, noseLowerCenter },
-        {
-          leftEyeCenter: calibratedLeftEyeCenter,
-          rightEyeCenter: calibratedRightEyeCenter,
-          noseLowerCenter: calibratedNoseLowerCenter,
-        },
-      );
-
-      if (greenPoints.length !== sharedRelativeVectors.value.length) {
-        safeSetFaceAlignedText(
-          isAligned
-            ? "Facial triangle aligned. Incorrect number of markers detected."
-            : "Align facial triangle. Incorrect number of markers detected.",
-        );
-      } else {
-        const tiltDirections = getMarkerTiltDirections(
-          greenPoints,
-          sharedRelativeVectors.value,
-          calibratedLeftEyeCenter,
-          calibratedRightEyeCenter,
-          calibratedNoseLowerCenter,
-        );
-
-        safeSetFaceAlignedText(
-          isAligned
-            ? tiltDirections.length > 0
-              ? `Facial triangle aligned. Tilt helmet ${tiltDirections.join(" and ")}`
-              : "All aligned"
-            : "Align facial triangle",
-        );
-      }
-
-      if (!sharedCalibratedBoxArea.value) {
-        return;
-      }
-      const threshold = 0.3;
-      const boxAreaRatio = boxArea / sharedCalibratedBoxArea.value;
-      const distanceFeedback = getDistanceFeedback(
-        boxAreaRatio,
-        threshold,
-        feedbackText,
-      );
-      // safeSetFeedBackText(distanceFeedback);
-
-      // Draw points using relative vectors
-      paint.setColor(Skia.Color("red"));
-      drawVectorPoints(
-        frame,
-        paint,
-        sharedRelativeVectors.value,
-        leftEyeCenter,
-        rightEyeCenter,
-        noseLowerCenter,
-      );
-    }
-
-    // console.log(points.leftPupil, points.rightPupil);
-    // const leftPupil = points.leftPupil[0];
-    // const rightPupil = points.rightPupil[0];
-    // paint.setColor(Skia.Color("red"));
-    // frame.drawCircle(leftPupil.x, leftPupil.y, 5, paint);
-    // frame.drawCircle(rightPupil.x, rightPupil.y, 5, paint);
-  }, []);
 
   if (!hasPermission)
     return <Button title="Request permission" onPress={requestPermission} />;
@@ -380,12 +159,14 @@ export default function App() {
         </Picker>
       </View>
 
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive
-        frameProcessor={skiaFrameProcessor}
-        format={format}
+      <CameraFeed
+        safeSetFaceAlignedText={safeSetFaceAlignedText}
+        safeSetFeedBackText={safeSetFeedBackText}
+        sharedCalibratedPoints={sharedCalibratedPoints}
+        sharedCalibratedBoxArea={sharedCalibratedBoxArea}
+        sharedShouldCalibrate={sharedShouldCalibrate}
+        sharedRelativeVectors={sharedRelativeVectors}
+        safeSetShowCalibrationModal={safeSetShowCalibrationModal}
       />
 
       {feedbackTexts.map((text, i) => (
